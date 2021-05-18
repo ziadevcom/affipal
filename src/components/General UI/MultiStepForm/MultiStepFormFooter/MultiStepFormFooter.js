aa4fde89569273647e262a81953e45e364d0b0c2
Zia <iloveeidhi@gmail.com>
2021-05-19 03:03:03 +0500

    form in progress

--- src/components/General UI/MultiStepForm/MultiStepFormFooter/MultiStepFormFooter.js
+++ src/components/General UI/MultiStepForm/MultiStepFormFooter/MultiStepFormFooter.js
@@ -3,11 +3,11 @@ import Button from "../../Button/Button";
 function MultiStepFormFooter() {
   return (
     <div className="multistep-form__footer">
-      <Button>
-        <img src="/next.svg" alt="" />
-      </Button>
       <Button variant="white">
         <img src="/back.svg" alt="" />
+      </Button>
+      <Button>
+        <img src="/next.svg" alt="" />
       </Button>
     </div>
   );
